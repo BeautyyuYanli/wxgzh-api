b5e43dacc4f2da5891d226d87f5c917b15d69c50
beautyyuyanli <beautyyuyanli@outlook.com>
2021-07-17 00:34:19 +0800

    update response header

--- listen.py
+++ listen.py
@@ -5,25 +5,34 @@ from urllib.parse import unquote
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "application/json")
-        self.end_headers()
         try:
             subscribe_list = self.path.split('query=')[1]
             subscribe_list = unquote(subscribe_list)
             subscribe_list = subscribe_list.split('$')
         except:
+            self.send_response(400)
+            self.send_header("Content-Type", "text/html; charset=UTF-8")
+            self.end_headers()
             self.wfile.write(bytes("parameters error!", "utf-8"))
         else:
             # value = update(subscribe_list)
             try:
                 value = update(subscribe_list)
             except:
+                self.send_response(500)
+                self.send_header("Content-Type", "text/html; charset=UTF-8")
+                self.end_headers()
                 self.wfile.write(bytes("update error!", "utf-8"))
             else:
                 if type(value).__name__ == 'str':
+                    self.send_response(500)
+                    self.send_header("Content-Type", "text/html; charset=UTF-8")
+                    self.end_headers()
                     self.wfile.write(bytes(value, "utf-8"))
                 else:
+                    self.send_response(200)
+                    self.send_header("Content-Type", "application/json")
+                    self.end_headers()
                     self.wfile.write(bytes(json.dumps(value), "utf-8"))
 httpd = socketserver.TCPServer(("", 11459), MyHandler)
 httpd.serve_forever()
